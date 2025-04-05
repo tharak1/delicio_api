8ed4b3a392239a4344fa82187fdd0172c9e7400d
Sai Tharak <saitharakreddyv59@gmail.com>
2025-04-05 10:31:24 +0530

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   src/index.ts 	new file:   tsconfig.json

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript Express!');
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
